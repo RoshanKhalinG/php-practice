46325b9599e77438a5680ccc0050cf27f8153eac
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-04-15 21:12:43 +0545

    Add files via upload

---
+++ Web 2/first.php
@@ -0,0 +1,16 @@
+<?php
+  
+ /* echo("Hello\n");
+  print("World");*/
+
+  //VARIABLE
+  /*$X=50;
+  echo $X;*/
+
+  //PRINT DATE & TIME
+//   echo date("d/m/y\n");
+//   print date("d.m.y");
+
+date_default_timezone_set("Asia/Kathmandu");
+echo date("h:i:s");
+?>
