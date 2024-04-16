01fb651a1736bc6bb2b99679f298cb714299d271
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-04-16 22:37:28 +0545

    2nd

--- Web 2/first.php
+++ Web 2/first.php
@@ -1,15 +1,15 @@
 <?php
   
- echo("Hello\n");
-  print("World");
+ /* echo("Hello\n");
+  print("World");*/
 
   //VARIABLE
-  $X=50;
-  echo $X;
+  /*$X=50;
+  echo $X;*/
 
   //PRINT DATE & TIME
-  echo date("d/m/y\n");
-  print date("d.m.y");
+//   echo date("d/m/y\n");
+//   print date("d.m.y");
 
 date_default_timezone_set("Asia/Kathmandu");
 echo date("h:i:s");
---
+++ Web 2/second.php
@@ -0,0 +1,27 @@
+<?php
+// echo "hello\n";
+// $x= 15;
+// $y;
+// echo $x;
+// echo date("\nd/m/y \n");
+// echo date("d.m.y \n");
+
+// date_default_timezone_set("Asia/Kathmandu");
+// echo date("F D,Y h:i:s A P " );
+echo "hello".$y."send";
+$var1=10;
+
+
+function xyz(){
+    $var1=10;
+    if($var1==10){
+        echo "satisfy".$var1;
+    }
+    else{
+        echo "no";
+    }
+    echo "hy how are you".$var1;
+}
+xyz();
+//echo $var1;
+?>
